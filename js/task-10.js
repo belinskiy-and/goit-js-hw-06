60b4ed8eca23595897a25fa74b6b681d9b599805
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-05-27 09:21:07 +0300

    Add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesElement = document.querySelector("#boxes");
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const numberElement = document.querySelector('input[type="number"]');
+
+const createBoxes = amount => {
+  let boxSize = 30;
+
+  for (let i = 0; i < +numberElement.value; i++) {
+    const newElement = document.createElement("div");
+    
+    newElement.classList.add("boxes__item");
+    newElement.style.width = boxSize + "px";
+    newElement.style.height = boxSize + "px";
+    newElement.style.backgroundColor = getRandomHexColor();
+    
+    boxSize += 10;
+
+    boxesElement.append(newElement);
+  }
+};
+
+const destroyBoxes = () => {
+  const boxesItems = document.querySelectorAll(".boxes__item");
+
+  for (const item of boxesItems) {
+    item.remove();  
+  }
+};
+
+btnCreate.addEventListener("click", event => createBoxes());
+
+btnDestroy.addEventListener("click", event => destroyBoxes());
